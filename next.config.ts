67f1876887af58edc942201262d1c6dbcd56030d
Reyn Macasaet <android.hashta@gmail.com>
2026-02-17 21:44:49 +0000

    still not there dude

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'irp.cdn-website.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
