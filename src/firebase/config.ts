d750313faa02a54249e94c1b551d5a190e10680f
Reyn Macasaet <android.hashta@gmail.com>
2026-02-19 16:01:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2232077555-525b9",
+  "appId": "1:599689968503:web:20a6ba31e53829ccffbf72",
+  "apiKey": "AIzaSyDNuCbm_niXUv_oHS6zG4WkHKXk9Lw-3ag",
+  "authDomain": "studio-2232077555-525b9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "599689968503"
+};
