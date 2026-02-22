739400c6d3762d27175a95e2c579384615638775
Reyn Macasaet <android.hashta@gmail.com>
2026-02-22 06:11:16 +0000

    File changes

--- next.config.ts
+++ next.config.ts
@@ -60,7 +60,9 @@ const runtimeCaching = [
 const pwaWrapper = withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  runtimeCaching,
+  workboxOptions: {
+    runtimeCaching,
+  }
 });
 
 const nextConfig: NextConfig = {
